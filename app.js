c8f736abfc92a69b1ed784aad5578610257a9ace
idiomasocw <idiomasocw@gmail.com>
2023-11-17 03:39:30 -0500

    server configuration correction

--- app.js
+++ app.js
@@ -6,6 +6,6 @@ app.use(express.static('public'));
 // Additional routes and server setup...
 
 const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => {
+app.listen(3000, '0.0.0.0', () => {
   console.log(`Server is running on port ${PORT}`);
 });
