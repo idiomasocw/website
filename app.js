27523e0582cf8cd9200b2ede2b78bc11ce8a8b1b
idiomasocw <idiomasocw@gmail.com>
2023-12-06 21:44:45 -0500

    priceManager has been implemented

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const path = require('path');
+const fs = require('fs');
 const app = express();
 require('dotenv').config();
 
@@ -7,51 +8,44 @@ require('dotenv').config();
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'));
 
-// Require additional modules for security and rate limiting
+// Additional modules for security and rate limiting
 const helmet = require('helmet');
 const rateLimit = require('express-rate-limit');
 
 const mailchimpRoutes = require('./routes/mailchimpRoutes');
 const getWordOfTheDay = require('./wordOfTheDay');
 
-// Apply security enhancements with Helmet, disabling CSP
-app.use(helmet({
-  contentSecurityPolicy: false, // Disable CSP
-}));
+// Security enhancements with Helmet, disabling CSP
+app.use(helmet({ contentSecurityPolicy: false }));
 
-// Apply rate limiting to specific routes
+// Rate limiting
 const limiter = rateLimit({
-  windowMs: 15 * 60 * 1000, // 15 minutes
-  max: 100, // limit each IP to 100 requests per windowMs
-  standardHeaders: true, // Return rate limit info in the `RateLimit-*` headers
-  legacyHeaders: false, // Disable the `X-RateLimit-*` headers
+  windowMs: 15 * 60 * 1000,
+  max: 100,
+  standardHeaders: true,
+  legacyHeaders: false,
 });
-app.use('/api', limiter); // Apply the rate limiter only to your API routes
+app.use('/api', limiter);
 
 // Serve static files
 app.use(express.static('public'));
 
-// Routes to render EJS templates
-app.get('/', (req, res) => {
-  res.render('index');
-});
+// Routes for EJS templates
+app.get('/', (req, res) => { res.render('index'); });
+
+// Read pricing data and pass it to cursos.ejs
 app.get('/cursos', (req, res) => {
-  res.render('cursos');
+  const pricingData = JSON.parse(fs.readFileSync(path.join(__dirname, 'pricing.json'), 'utf-8'));
+  res.render('cursos', { pricing: pricingData });
 });
 
-app.get('/terminos-y-condiciones', (req, res) => {
-  res.render('terminos-y-condiciones');
-});
-app.get('/privacidad', (req, res) => {
-  res.render('privacidad');
-});
-app.get('/thankyou', (req, res) => {
-  res.render('thankyou');
-});
+app.get('/terminos-y-condiciones', (req, res) => { res.render('terminos-y-condiciones'); });
+app.get('/privacidad', (req, res) => { res.render('privacidad'); });
+app.get('/thankyou', (req, res) => { res.render('thankyou'); });
 
 app.get('/word-of-the-day', (req, res) => {
-  const wordData = getWordOfTheDay(); // Get the word data
-  res.render('wordOfTheDay', { word: wordData }); // Pass the data to the EJS template
+  const wordData = getWordOfTheDay(); 
+  res.render('wordOfTheDay', { word: wordData });
 });
 
 app.use(express.json());
@@ -59,10 +53,26 @@ app.use(express.json());
 // Prefix routes with '/api'
 app.use('/api', mailchimpRoutes);
 
-// 404 Error Handling
-app.use((req, res, next) => {
-  res.status(404).render('404'); // Render a 404.ejs page if you have one, or send a 404 message
+// Pricing manager routes - Modified to read pricing data
+app.get('/price&discount-manager', (req, res) => {
+  fs.readFile(path.join(__dirname, 'pricing.json'), 'utf8', (err, data) => {
+    if (err) {
+      console.error("Error reading pricing file:", err);
+      return res.status(500).send("Error loading pricing information");
+    }
+    const pricing = JSON.parse(data);
+    res.render('priceManager', { pricing });
+  });
 });
+
+app.post('/price&discount-manager', express.urlencoded({ extended: true }), (req, res) => {
+  const newPricing = req.body;
+  fs.writeFileSync(path.join(__dirname, 'pricing.json'), JSON.stringify(newPricing, null, 2));
+  res.redirect('/price&discount-manager');
+});
+
+// 404 Error Handling
+app.use((req, res, next) => { res.status(404).render('404'); });
 
 // General Error Handling
 app.use((error, req, res, next) => {
