2ba1db2849049c5db637c74c6bb5ff1c15c733ee
Diego E <diego28e@gmail.com>
2024-07-23 16:47:55 -0500

    certificate svg added and lambda function trigger button added to admin dashboard

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,29 +1,33 @@
-const AWS = require('aws-sdk');
-const dotenv = require('dotenv');
+const AWS = require("aws-sdk");
+const dotenv = require("dotenv");
+const axios = require("axios");
 
 dotenv.config();
 
 const cognito = new AWS.CognitoIdentityServiceProvider({
-  region: process.env.AWS_REGION
+  region: process.env.AWS_REGION,
 });
 
 const dynamoDb = new AWS.DynamoDB.DocumentClient({
-  region: process.env.AWS_REGION
+  region: process.env.AWS_REGION,
 });
 
 const s3 = new AWS.S3({
-  region: process.env.AWS_REGION
+  region: process.env.AWS_REGION,
 });
+
+const apiGatewayUrl =
+  "https://yeeq44bp8h.execute-api.us-east-1.amazonaws.com/sync-users"; // Here's the API Gateway URL
 
 const sendLoginCode = async (req, res) => {
   const email = req.body.email;
 
   const params = {
-    AuthFlow: 'CUSTOM_AUTH',
+    AuthFlow: "CUSTOM_AUTH",
     ClientId: process.env.COGNITO_CLIENT_ID,
     AuthParameters: {
-      USERNAME: email
-    }
+      USERNAME: email,
+    },
   };
 
   try {
@@ -31,12 +35,13 @@ const sendLoginCode = async (req, res) => {
     req.session.challengeName = authResponse.ChallengeName;
     req.session.session = authResponse.Session;
     req.session.email = email;
-    req.session.message = "We have sent a One-Time Password to your email. Check your email 'spam' or 'junk' folder if necessary.";
+    req.session.message =
+      "We have sent a One-Time Password to your email. Check your email 'spam' or 'junk' folder if necessary.";
     res.redirect(`/verify-code?email=${encodeURIComponent(email)}`);
   } catch (error) {
-    console.error('Error sending login code:', error);
+    console.error("Error sending login code:", error);
     req.session.error = "The email that you entered has no access privileges.";
-    res.redirect('/login');
+    res.redirect("/login");
   }
 };
 
@@ -49,72 +54,93 @@ const verifyLoginCode = async (req, res) => {
     ClientId: process.env.COGNITO_CLIENT_ID,
     ChallengeResponses: {
       USERNAME: email,
-      ANSWER: code
+      ANSWER: code,
     },
-    Session: req.session.session
+    Session: req.session.session,
   };
 
   try {
     const data = await cognito.respondToAuthChallenge(params).promise();
-    if (data.AuthenticationResult && data.AuthenticationResult.IdToken && data.AuthenticationResult.AccessToken) {
+    if (
+      data.AuthenticationResult &&
+      data.AuthenticationResult.IdToken &&
+      data.AuthenticationResult.AccessToken
+    ) {
       const accessToken = data.AuthenticationResult.AccessToken;
 
       // Fetch user groups
       const userGroupsParams = {
-        AccessToken: accessToken
+        AccessToken: accessToken,
       };
       const userData = await cognito.getUser(userGroupsParams).promise();
       const groupsParams = {
         UserPoolId: process.env.COGNITO_USER_POOL_ID,
-        Username: userData.Username
+        Username: userData.Username,
       };
-      const userGroups = await cognito.adminListGroupsForUser(groupsParams).promise();
+      const userGroups = await cognito
+        .adminListGroupsForUser(groupsParams)
+        .promise();
 
       // Determine role based on group membership
-      const userRole = userGroups.Groups.length > 0 ? userGroups.Groups[0].GroupName : 'students'; // Default to 'students' if no group
+      const userRole =
+        userGroups.Groups.length > 0
+          ? userGroups.Groups[0].GroupName
+          : "students"; // Default to 'students' if no group
 
-      req.session.role = userRole;  // Store role in session
+      req.session.role = userRole; // Store role in session
 
       // Log the stored email and role for verification
-      console.log(`verifyLoginCode - Email stored in session: ${req.session.email}`);
-      console.log(`verifyLoginCode - Role stored in session: ${req.session.role}`);
+      console.log(
+        `verifyLoginCode - Email stored in session: ${req.session.email}`
+      );
+      console.log(
+        `verifyLoginCode - Role stored in session: ${req.session.role}`
+      );
 
       // Check group membership and redirect accordingly
-      const isAdmin = userGroups.Groups.some(group => group.GroupName === 'admins');
+      const isAdmin = userGroups.Groups.some(
+        (group) => group.GroupName === "admins"
+      );
       if (isAdmin) {
-        res.cookie('idToken', data.AuthenticationResult.IdToken, { httpOnly: true });
-        res.cookie('accessToken', accessToken, { httpOnly: true });
-        return res.redirect('/priceManager');
+        res.cookie("idToken", data.AuthenticationResult.IdToken, {
+          httpOnly: true,
+        });
+        res.cookie("accessToken", accessToken, { httpOnly: true });
+        return res.redirect("/priceManager");
       } else {
-        res.cookie('idToken', data.AuthenticationResult.IdToken, { httpOnly: true });
-        res.cookie('accessToken', accessToken, { httpOnly: true });
-        return res.redirect('/studentDashboard');
+        res.cookie("idToken", data.AuthenticationResult.IdToken, {
+          httpOnly: true,
+        });
+        res.cookie("accessToken", accessToken, { httpOnly: true });
+        return res.redirect("/studentDashboard");
       }
     } else {
-      console.error('Authentication result does not contain tokens', data);
+      console.error("Authentication result does not contain tokens", data);
       req.session.error = "Failed to verify the code. Please try again.";
       res.redirect(`/verify-code?email=${encodeURIComponent(email)}`);
     }
   } catch (error) {
-    console.error('Error verifying login code:', error);
+    console.error("Error verifying login code:", error);
     req.session.error = "Failed to verify the code. Please try again.";
     res.redirect(`/verify-code?email=${encodeURIComponent(email)}`);
   }
 };
 
 const fetchUserData = async (email, role) => {
-  console.log(`fetchUserData - Fetching user data for email: ${email}, role: ${role}`);
+  console.log(
+    `fetchUserData - Fetching user data for email: ${email}, role: ${role}`
+  );
   const params = {
-    TableName: 'users',
+    TableName: "users",
     Key: {
       email: email,
-      role: role
-    }
+      role: role,
+    },
   };
 
   try {
     const data = await dynamoDb.get(params).promise();
-    console.log('fetchUserData - DynamoDB get response:', data);
+    console.log("fetchUserData - DynamoDB get response:", data);
     if (data.Item) {
       console.log(`User data for ${email}:`, data.Item);
       return data.Item;
@@ -123,7 +149,7 @@ const fetchUserData = async (email, role) => {
       return null;
     }
   } catch (error) {
-    console.error('Error fetching user data from DynamoDB:', error);
+    console.error("Error fetching user data from DynamoDB:", error);
     return null;
   }
 };
@@ -132,17 +158,21 @@ const showDashboard = async (req, res) => {
   const accessToken = req.cookies.accessToken;
 
   if (!accessToken) {
-    return res.redirect('/login');
+    return res.redirect("/login");
   }
 
   try {
     const params = {
-      AccessToken: accessToken
+      AccessToken: accessToken,
     };
 
     const userData = await cognito.getUser(params).promise();
-    const givenName = userData.UserAttributes.find(attr => attr.Name === 'given_name').Value;
-    const familyName = userData.UserAttributes.find(attr => attr.Name === 'family_name').Value;
+    const givenName = userData.UserAttributes.find(
+      (attr) => attr.Name === "given_name"
+    ).Value;
+    const familyName = userData.UserAttributes.find(
+      (attr) => attr.Name === "family_name"
+    ).Value;
     const fullName = `${givenName} ${familyName}`;
 
     const email = req.session.email;
@@ -150,10 +180,10 @@ const showDashboard = async (req, res) => {
 
     const userDetails = await fetchUserData(email, role);
 
-    res.render('studentDashboard', { fullName, userDetails });
+    res.render("studentDashboard", { fullName, userDetails });
   } catch (error) {
-    console.error('Error fetching user data:', error);
-    res.redirect('/login');
+    console.error("Error fetching user data:", error);
+    res.redirect("/login");
   }
 };
 
@@ -162,22 +192,32 @@ const generatePresignedUrl = async (req, res) => {
   const params = {
     Bucket: process.env.S3_BUCKET_NAME, // Ensure this is set in your .env
     Key: key,
-    Expires: 5 * 60 // URL expiry time in seconds (5 minutes)
+    Expires: 5 * 60, // URL expiry time in seconds (5 minutes)
   };
 
   try {
-    const url = s3.getSignedUrl('getObject', params);
+    const url = s3.getSignedUrl("getObject", params);
     res.json({ url });
   } catch (error) {
-    console.error('Error generating presigned URL:', error);
-    res.status(500).json({ error: 'Error generating presigned URL' });
+    console.error("Error generating presigned URL:", error);
+    res.status(500).json({ error: "Error generating presigned URL" });
+  }
+};
+
+const triggerLambdaFunction = async (req, res) => {
+  try {
+    const response = await axios.post(apiGatewayUrl);
+    res.json({ success: true, data: response.data });
+  } catch (error) {
+    console.error("Error triggering Lambda function:", error);
+    res.status(500).json({ success: false, message: error.message });
   }
 };
 
 const logout = (req, res) => {
-  res.clearCookie('idToken');
-  res.clearCookie('accessToken');
-  res.redirect('/');
+  res.clearCookie("idToken");
+  res.clearCookie("accessToken");
+  res.redirect("/");
 };
 
 module.exports = {
@@ -185,5 +225,6 @@ module.exports = {
   verifyLoginCode,
   showDashboard,
   generatePresignedUrl,
-  logout
+  triggerLambdaFunction,
+  logout,
 };
--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -1,38 +1,50 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
-const { sendLoginCode, verifyLoginCode, showDashboard, generatePresignedUrl, logout } = require('../controllers/userController');
-const { getPricingData, getPriceDiscountManager } = require('../controllers/pricingController');
+const {
+  sendLoginCode,
+  verifyLoginCode,
+  showDashboard,
+  generatePresignedUrl,
+  triggerLambdaFunction,
+  logout,
+} = require("../controllers/userController");
+const {
+  getPricingData,
+  getPriceDiscountManager,
+} = require("../controllers/pricingController");
 
 // Routes
-router.get('/login', (req, res) => {
+router.get("/login", (req, res) => {
   const error = req.session.error;
   req.session.error = null; // Clear the error after displaying it
-  res.render('login', { error });
+  res.render("login", { error });
 });
 
-router.post('/send-login-code', sendLoginCode);
+router.post("/send-login-code", sendLoginCode);
 
-router.get('/send-login-code', (req, res) => {
+router.get("/send-login-code", (req, res) => {
   req.body.email = req.query.email;
   sendLoginCode(req, res);
 });
 
-router.get('/verify-code', (req, res) => {
+router.get("/verify-code", (req, res) => {
   const message = req.session.message;
   const error = req.session.error;
   req.session.message = null; // Clear the message after displaying it
   req.session.error = null; // Clear the error after displaying it
-  res.render('verify-code', { email: req.query.email, message, error });
+  res.render("verify-code", { email: req.query.email, message, error });
 });
 
-router.post('/verify-login-code', verifyLoginCode);
+router.post("/verify-login-code", verifyLoginCode);
 
-router.get('/studentDashboard', showDashboard);
+router.get("/studentDashboard", showDashboard);
 
-router.get('/generatePresignedUrl', generatePresignedUrl); // Add the route for presigned URL generation
+router.get("/generatePresignedUrl", generatePresignedUrl); // Add the route for presigned URL generation
 
-router.get('/priceManager', getPriceDiscountManager); // Use existing controller function to fetch and render pricing data
+router.get("/priceManager", getPriceDiscountManager); // Use existing controller function to fetch and render pricing data
 
-router.get('/logout', logout);
+router.post("/trigger-lambda", triggerLambdaFunction); // Add the route for triggering the Lambda function
+
+router.get("/logout", logout);
 
 module.exports = router;
